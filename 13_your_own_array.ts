f3e436105a8709101eaaef14939118bf3207146d
MoeezAftab <163597623+MoeezAftab@users.noreply.github.com>
2024-04-25 16:51:34 +0500

    Add files via upload

---
+++ 13_your_own_array.ts
@@ -0,0 +1,5 @@
+// exercise 13
+let transportation : string [] = [`honda`,`civic`,`bus`,`bike`];
+for(let i=0; i<transportation.length; i++){
+    console.log('i would like to own a' + transportation[i]);
+}
