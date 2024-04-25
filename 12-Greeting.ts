53a1a684b0caa582805ef760d1508d9452cb5ae5
MoeezAftab <163597623+MoeezAftab@users.noreply.github.com>
2024-04-25 16:50:35 +0500

    Add files via upload

---
+++ 12-Greeting.ts
@@ -0,0 +1,5 @@
+let members: string[] =[`Moeez`,`Zubair`,`Zain`,`Zohaib`];
+let message: string=`kal kis ki birthday ha:`;
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
