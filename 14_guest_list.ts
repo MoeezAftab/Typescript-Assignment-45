09863e1d38fc87eb0dbe596cbecdd9a376ed08c0
MoeezAftab <163597623+MoeezAftab@users.noreply.github.com>
2024-04-25 16:52:32 +0500

    Add files via upload

---
+++ 14_guest_list.ts
@@ -0,0 +1,4 @@
+let guest_list : string [] = [`Ali`,`Asim`,`Anaya`,`Kashaf`];
+for(let i=0; i<guest_list.length; i++){
+    console.log('kal khhane par ajana ' + guest_list[i] + ',\nor han lifafa nhu bhoolna warna wapis nikal lena');
+}
