afc10b9aec6efd9cfc9c3e46b3b90cb08517c8a1
MoeezAftab <163597623+MoeezAftab@users.noreply.github.com>
2024-04-25 16:54:24 +0500

    Add files via upload

---
+++ 16_More_Guest.ts
@@ -0,0 +1,22 @@
+let guest_list = [`Ali`, `Asim`, `Anaya`, `Kashaf`];
+let not_present = 'Asim';
+let new_guest = 'Adeena';
+guest_list[1] = new_guest;
+
+for (let i = 0; i <guest_list.length; i++) {
+    console.log('sir/mam ' + guest_list[i] + ',\n\nwe invited you on birthday party tomorrow.\n\nthankyou\n');
+
+}
+console.log(`Mr. ${not_present} will not come totmorrow birhday party.`);
+
+console.log('good news! We find a big table so we are invting 3 more guest.')
+
+
+guest_list.unshift('Zubair');
+guest_list.splice(2 , 0 , 'Zain');
+guest_list.push('Zohaib');
+
+for (let i = 0; i <guest_list.length; i++) {
+    console.log('sir/mam ' + guest_list[i] + ',\n\nwe invited you on birthday party tomorrow.\n\nthankyou\n');
+
+}
