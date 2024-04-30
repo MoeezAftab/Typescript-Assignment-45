47a25593b0f2c6cdb0a64292748b0eca59aba386
MoeezAftab <163597623+MoeezAftab@users.noreply.github.com>
2024-04-30 10:50:43 +0500

    Add files via upload

---
+++ 3.title_case.ts
@@ -0,0 +1,6 @@
+let personalName : string = "Moeez";
+
+console.log("lowerCase", personalName.toLowerCase());
+
+console.log("uppercase", personalName.toUpperCase());
+console.log(`titlecase:  ${personalName.charAt(0).toUpperCase() + personalName.slice(1).toLowerCase()}`);
